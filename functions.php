b5d44bd8071e3dd45fe70adf06e3b188244f2a8d
Val Heimpel <vheimpel@gmail.com>
2018-11-25 13:31:32 -0500

    Adds tachyons and custom stylesheet

--- functions.php
+++ functions.php
@@ -122,6 +122,8 @@ add_action( 'widgets_init', 'nomad_moon_widgets_init' );
 function nomad_moon_scripts() {
 	wp_enqueue_style( 'nomad-moon-style', get_stylesheet_uri() );
 
+	wp_enqueue_style( 'nomad-moon-custom', get_template_directory_uri() . '/css/custom.css');
+
 	wp_enqueue_script( 'nomad-moon-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'nomad-moon-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
--- header.php
+++ header.php
@@ -17,6 +17,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
+	<link rel="stylesheet" href="https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css"/>
 	<?php wp_head(); ?>
 </head>
 
